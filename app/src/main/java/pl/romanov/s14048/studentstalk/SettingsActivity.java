4284a27bd581f20e0b615305f8d5882e9539a4bd
romanov1589 <35072304+romanov1589@users.noreply.github.com>
2018-06-15 21:21:44 +0200

    Storing profile image into Firebase Storage

--- app/src/main/java/pl/romanov/s14048/studentstalk/SettingsActivity.java
+++ app/src/main/java/pl/romanov/s14048/studentstalk/SettingsActivity.java
@@ -2,18 +2,25 @@ package pl.romanov.s14048.studentstalk;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
@@ -29,6 +36,8 @@ public class SettingsActivity extends AppCompatActivity {
 
     private final static int galleryPick = 1;
 
+    private StorageReference storeProfileImageStorageRef;
+
     private DatabaseReference getUserDataReference;
     private FirebaseAuth mAuth;
 
@@ -42,6 +51,7 @@ public class SettingsActivity extends AppCompatActivity {
         String onlineUserId = mAuth.getCurrentUser().getUid();
 
         getUserDataReference = FirebaseDatabase.getInstance().getReference().child("Users").child(onlineUserId);
+        storeProfileImageStorageRef = FirebaseStorage.getInstance().getReference().child("Profile_Images");
 
         settingsDispayProfileImage = (CircleImageView) findViewById(R.id.settings_profile_image);
         settingsDisplayName = (TextView) findViewById(R.id.settings_username);
@@ -59,7 +69,7 @@ public class SettingsActivity extends AppCompatActivity {
 
                 settingsDisplayName.setText(name);
                 settingsDisplayStatus.setText(status);
-                //TODO
+
             }
 
             @Override
@@ -89,6 +99,28 @@ public class SettingsActivity extends AppCompatActivity {
                     .setGuidelines(CropImageView.Guidelines.ON)
                     .setAspectRatio(1,1)
                     .start(this);
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                String userId = mAuth.getCurrentUser().getUid();
+                StorageReference filePath = storeProfileImageStorageRef.child(userId + ".jpg");
+                filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(SettingsActivity.this, "Saving your profile Image to Firebase Storage...",
+                                    Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(SettingsActivity.this, "Error ocured, while uploading your Profile Pic",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
         }
     }
 }
