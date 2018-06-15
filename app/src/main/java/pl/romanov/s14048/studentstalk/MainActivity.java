5ddc24f59871d901bec22d6bf3aba2028b75fabf
romanov1589 <35072304+romanov1589@users.noreply.github.com>
2018-06-15 23:04:01 +0200

    All Users Activity Added

--- app/src/main/java/pl/romanov/s14048/studentstalk/MainActivity.java
+++ app/src/main/java/pl/romanov/s14048/studentstalk/MainActivity.java
@@ -76,6 +76,11 @@ public class MainActivity extends AppCompatActivity {
             Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
             startActivity(settingsIntent);
         }
+
+        if(item.getItemId() == R.id.main_all_users_button){
+            Intent allUsersIntent = new Intent(MainActivity.this, AllUsersActivity.class);
+            startActivity(allUsersIntent);
+        }
         return true;
     }
 }
